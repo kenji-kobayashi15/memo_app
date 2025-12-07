4150c863d2e32cb079d10821506adccfbc7dee29
kenji-kobayashi15 <kenzicolab.future@gmail.com>
2025-12-07 10:22:24 +0900

    詳細画面と一覧画面完了

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\volt\volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// 一覧ページのルーティング
+volt::route('/memos', 'memos.index')->name('memos.index');
+// コンポーネントはseil artisan make:volt index
+// ブラウザへの表示は/memosの部分となる
+
+// 詳細ページのルーティング作業1 次にコンポーネント作成
+volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
+// コンポーネントは sail artisan make:volt memos/show としている。
+// ブラウザへの表示は/memos番号となる。この番号は象さんマークの番号且つ内容はメモシーダーphpからから持ってきている。
